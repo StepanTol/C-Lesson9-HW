fcda935a20408bdf1a462d5fde484a407f804634
Stepan <tstepa037@gmail.com>
2023-01-24 08:50:50 +0300

    task67

---
+++ task67/Program.cs
@@ -0,0 +1,19 @@
+﻿//Задача 67: Напишите программу, которая будет принимать на вход число и 
+//возвращать сумму его цифр.
+//453 -> 12
+//45 -> 9
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+int SumNumbers(int num)
+{
+    if (num == 0)
+    {
+        return 0;
+    }
+
+    return(num % 10 + SumNumbers(num / 10));
+}
+
+Console.WriteLine(SumNumbers(N));
