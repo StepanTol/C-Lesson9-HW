28eb612b29fdb6378d774e811d6c1d939c776db5
Stepan <tstepa037@gmail.com>
2023-01-24 08:49:53 +0300

    task63

---
+++ task63/Program.cs
@@ -0,0 +1,15 @@
+﻿//Задача 63: Задайте значение N. Напишите программу, которая выведет 
+//все натуральные числа в промежутке от 1 до N.
+//N = 5 -> "1, 2, 3, 4, 5"
+//N = 6 -> "1, 2, 3, 4, 5, 6"
+
+
+string NaturalN(int start, int end)
+{
+    if (start == end)
+    {
+        return start.ToString(); 
+    }
+    return(start+" "+NaturalN(start+1,end));
+}
+Console.WriteLine(NaturalN(1,5));
